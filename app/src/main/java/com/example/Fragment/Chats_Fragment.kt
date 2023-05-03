3c45e7315fc8146d2e29fe1233e4b4c56c281dbf
pratik <pratikgangadiya465@gmail.com>
2023-05-03 12:02:23 +0530

    commit8

--- app/src/main/java/com/example/Fragment/Chats_Fragment.kt
+++ app/src/main/java/com/example/Fragment/Chats_Fragment.kt
@@ -1,13 +1,24 @@
 package com.example.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.whatsapp.R
+import com.example.whatsapp.R.drawable.City1
+import com.example.whatsapp.R.drawable.City2
+import com.example.whatsapp.R.drawable.City3
+import com.example.whatsapp.R.drawable.City4
+import com.example.whatsapp.R.drawable.City5
+import com.example.whatsapp.R.drawable.City6
+import com.example.whatsapp.R.drawable.City7
+import com.example.whatsapp.R.drawable.City8
 
 class Chats_Fragment : Fragment() {
+
+    var  image = arrayOf(City1,City2,City3,City4,City5,City6,City7,City8)
+    var name = arrayOf( d)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
